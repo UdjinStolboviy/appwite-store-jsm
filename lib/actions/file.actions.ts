765ad5af6b7c27e4a0be43e0b59c2409ae7fe576
Udjin Stolboviy <jenyjenyy@gmail.com>
2025-01-06 12:44:15 +0100

    add fix

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -224,7 +224,7 @@ export async function getTotalSpaceUsed() {
       audio: { size: 0, latestDate: "" },
       other: { size: 0, latestDate: "" },
       used: 0,
-      all: 2 * 1024 * 1024 * 1024 /* 2GB available bucket storage */,
+      all: 300 * 1024 * 1024 * 1024 /* 2GB available bucket storage */,
     };
 
     files.documents.forEach((file) => {
